85e908a1463cc8e6f0a49c3d7284e4a1c4467190
John O'Brien <obrien.jk@gmail.com>
2015-07-11 00:43:09 +0100

    Remove border from legend in lower left plot

--- plot4.R
+++ plot4.R
@@ -58,6 +58,7 @@ lines(dTime, Sub_metering_3, col="blue")
 legend("topright",
        c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
        col=c("black", "red", "blue"),
+       bty="n",
        lty=par("lty")
 )
 
