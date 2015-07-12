b90c2b3ed466b052565f409d6b4bed4b9bab3fbb
John O'Brien <obrien.jk@gmail.com>
2015-07-12 15:00:34 +0100

    Make explanation of awk and pipe clearer.

--- plot4.R
+++ plot4.R
@@ -1,22 +1,23 @@
-# We start with the data file downloaded and unzipped.
+# We start with the data file already downloaded and unzipped.
 
 # Read the data using a pipe. This allows us to read in only the data lines
 # required for the assignment. As it happens, in this case, the whole data file
-# is not too big to fit in memory, but it's a useful exercise for when we do
-# encounter a file that won't all fit.
+# would fit in memory, but it's a useful exercise for when we do encounter a
+# file that's too big to read.
 
 # This is not a cross-platform solution, it depends on the presence of the awk
 # program, which is installed by default on most linux/unix platforms.
 
 
-# First construct our awk command.
+# First construct our awk command. This runs outside R and filters the data file
+# returning only the lines that we need.
 awk_cmd <- paste(
     "awk -F';' 'NR == 1 || $1 ==\"2/2/2007\" || $1 ==\"1/2/2007\" {print $0}'",
     "household_power_consumption.txt"
 )
 
 
-# Then make our pipe connection.
+# Then make our pipe connection. This allows R to read the output of awk above.
 pipe_con <- pipe(awk_cmd, open="r")
 
 
